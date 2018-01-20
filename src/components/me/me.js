7868843c1e4062f3af9297720e1fb8a4b0a2f8a1
Orlando Watson <ogw@orlandogwatson.com>
2018-01-19 21:46:31 -0800

    add more content

--- src/components/me/me.js
+++ src/components/me/me.js
@@ -10,8 +10,18 @@ export default class Me extends Component {
             <div>
                 <Header key="me--header" page="me" heading="I'm Orlando."/>
                 <div className="me">
-
+                    <span className="section--accent">
+                        <i data-feather="activity" width="32" height="32"></i>
+                    </span>
+                    <h1 className="me--heading">
+                        Saving the World, One Webpage at a Time.
+                    </h1>
+                    <p className="me--info">
+                        I am a self-taught, bachelor of science degree (Computer Science) having fullstack web developer. I specialize in web application and wesite design/development.
+                    </p>
                 </div>
+                <div className="me--sections-section"></div>
+                <div className="me--sections-section"></div>
             </div>
         )
     }
